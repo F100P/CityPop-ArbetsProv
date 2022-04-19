a0c54b86e47844b3d5f1a8ef1e0bf17ae48499b0
Tim <tim.j.myllyla@gmail.com>
2022-04-19 23:26:11 +0200

    basic navigation

--- App.tsx
+++ App.tsx
@@ -6,8 +6,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchByCity from './Screens/SearchByCity';
 import MainMenu from './Components/MainMenu';
 import SearchByCountry from './Screens/SearchByCountry';
-
+import ViewPopulation from './Screens/ViewPopulation';
+import CityFilter from './Screens/CityFilter';
 const RootStack = createNativeStackNavigator();
+
+//Work for tomorrow, keep watching video. nesled screens, Investigate API
 
 export default function App() {
   return (
@@ -15,6 +18,8 @@ export default function App() {
     <RootStack.Navigator>
     <RootStack.Screen name='SearchByCity' component={SearchByCity}/>
     <RootStack.Screen name='SearchByCountry' component={SearchByCountry}/>
+    <RootStack.Screen name='CityFilter' component={CityFilter}/>
+    <RootStack.Screen name='ViewPopultation' component={ViewPopulation}/>
       </RootStack.Navigator>
     <MainMenu />
     
--- Screens/SearchByCity.tsx
+++ Screens/SearchByCity.tsx
@@ -1,9 +1,13 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 const SearchByCity = () => {
+    const navigation = useNavigation();
     return (
         <View>
+           
             <Text>Place Holder</Text>
+            
         </View>
     );
 };
