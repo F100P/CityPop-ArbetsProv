e9b975061076ff02501e237f2de3c11acb6d6c13
Tim <tim.j.myllyla@gmail.com>
2022-04-20 00:05:08 +0200

    added react-native-papers. and searchbar

--- Screens/SearchByCity.tsx
+++ Screens/SearchByCity.tsx
@@ -1,10 +1,14 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Entypo} from "@expo/vector-icons"
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { Searchbar, Button } from 'react-native-paper';
 const SearchByCity = () => {
     const navigation = useNavigation();
     return (
         <View>
+           <Searchbar/>
+           <Entypo name="magnifying-glass" size={240} color="black" onPress={() => alert("ive been pressed")} />
            
             <Text>Place Holder</Text>
             
