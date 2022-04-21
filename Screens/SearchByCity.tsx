51c130781ffc0857e7af7e651dd6185efd21d7cd
F100P <tim.j.myllyla@gmail.com>
2022-04-21 22:47:20 +0200

    Snyggade till koden i SearchCity

--- Screens/SearchByCity.tsx
+++ Screens/SearchByCity.tsx
@@ -1,66 +1,50 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {Entypo} from "@expo/vector-icons"
-import React, {useEffect, useState} from 'react';
-import { ActivityIndicator, FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Searchbar, Button } from 'react-native-paper';
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Searchbar, Button } from "react-native-paper";
 import test from "../connections/test";
-import searchCity from '../connections/searchCity';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParams } from '../App';
-const SearchByCityScreen = ({navigation}) => {
-    
-  type Props = NativeStackScreenProps<RootStackParams, `SearchByCityScreen`>
-   
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-    const [testa, setTesta] = useState([]);
-    const [searchQuary, setSearchQuery] = useState([]);
-    const [country, setCountry] = useState([]);
-    const [population, setPopulation] = useState([]);
-    
-    
+import searchCity from "../connections/searchCity";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParams } from "../App";
+const SearchByCityScreen = ({ navigation }) => {
+  type Props = NativeStackScreenProps<RootStackParams, `SearchByCityScreen`>;
 
+  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [testa, setTesta] = useState([]);
+  const [searchQuary, setSearchQuery] = useState([]);
 
-    const [Country, setCoutry] = useState([]);
-    const onChangeSearch = query => setSearchQuery(query); //uppdaterar variabeln som ska skickas till Searchcountry
-    const SearchPressed = () =>  searchCity(searchQuary).then((data)=>{
-      console.log(data);
-     setPopulation(data.geonames[0].population);
-      setCountry(data.geonames[0].name);
+  const [Country, setCoutry] = useState([]);
+  const navigateToNext = (country, population) =>
+    navigation.navigate(`ViewPopultation`, {
+      name: country,
+      population: population,
+    });
+  const onChangeSearch = (query) => setSearchQuery(query); //uppdaterar variabeln som ska skickas till Searchcountry
 
-    }) //uppdaterar variabeln som ska skickas till Searchcountry
+  const SearchPressed = () =>
+    searchCity(searchQuary).then((data) => {
+      
+      navigateToNext(data.geonames[0].name, data.geonames[0].population);
+    }, setLoading(true) ); //Ge felmeddelande om navigeringen inte är okej
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      <Text>SEARCH BY CITY</Text>
+      <Searchbar
+        placeholder='Enter a city'
+        value={searchQuary}
+        onChangeText={onChangeSearch}
+      />
 
-     useEffect(() => {
-         
-
-          test().then((data)=>{setData(data.data);
-             // maska ur ett värde! yay
-           setTesta(data.geonames[0].countryName);
-            setLoading(false);
-            setTesta(data.geonames[0].countryName);
-        });
-        
-      }, []);
-    
-      return (
-        <View style={{ flex: 1, padding: 24 }}>
-          {isLoading ? <ActivityIndicator/> : (
-           <>
-           <Text>{country}</Text>
-           <Text>{population}</Text>
-           <Searchbar placeholder='Search Country' value={searchQuary} onChangeText={onChangeSearch} />{}</>
-          )}
-          <Entypo name="magnifying-glass" size={240} color="black"  onPress={SearchPressed} />
-          <Button onPress={()=>navigation.navigate(`ViewPopultation`, {name: country, population: population})} >hallå</Button>
-          <Text>{searchQuary}</Text>
-        </View>
-      );
-
-    };
-const styles = StyleSheet.create({
-
-});
+      <TouchableOpacity onPress={SearchPressed}>
+        <Entypo name='magnifying-glass' size={240} color='black' />
+      </TouchableOpacity>
+      {isLoading ? <Text>Din Sökning gav inget resultat försök igen</Text> : (null)}
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
 
 export default SearchByCityScreen;
-
--- Screens/SearchByCountry.tsx
+++ Screens/SearchByCountry.tsx
@@ -1,16 +1,60 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Entypo} from "@expo/vector-icons"
+import React, {useEffect, useState} from 'react';
+import { ActivityIndicator, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Searchbar, Button } from 'react-native-paper';
+import test from "../connections/test";
+import searchCity from '../connections/searchCity';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParams } from '../App';
+const SearchByCountry = ({navigation}) => {
+    
+  type Props = NativeStackScreenProps<RootStackParams, `SearchByCityScreen`>
+   
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [testa, setTesta] = useState([]);
+    const [searchQuary, setSearchQuery] = useState([]);
+    const [country, setCountry] = useState([]);
+    const [population, setPopulation] = useState([]);
+    
+    
 
-const SearchByCountry = () => {
-    return (
-        <View>
-            <Text>SeachByCountry</Text>
+
+    const [Country, setCoutry] = useState([]);
+    const onChangeSearch = query => setSearchQuery(query); //uppdaterar variabeln som ska skickas till Searchcountry
+    const SearchPressed = () =>  searchCity(searchQuary).then((data)=>{
+      console.log(data);
+     setPopulation(data.geonames[0].population);
+      setCountry(data.geonames[0].name);
+
+    }) //uppdaterar variabeln som ska skickas till Searchcountry
+
+     useEffect(() => {
+         
+
+       
+      }, []);
+    
+      return (
+        <View style={{ flex: 1, padding: 24 }}>
+          {isLoading ? <ActivityIndicator/> : (
+           <>
+           <Text>{country}</Text>
+           <Text>{population}</Text>
+           <Searchbar placeholder='Search Country' value={searchQuary} onChangeText={onChangeSearch} />{}</>
+          )}
+          <Entypo name="magnifying-glass" size={240} color="black"  onPress={SearchPressed} />
+          <Button onPress={()=>navigation.navigate(`ViewPopultation`, {name: country, population: population})} >hallå</Button>
+          <Text>{searchQuary}</Text>
         </View>
-    );
-};
+      );
 
+    };
 const styles = StyleSheet.create({
 
 });
 
 export default SearchByCountry;
+
